d43345b1655ffc13c28910c4885f374d746568d7
tcd4 <tcd4@njit.edu>
2015-06-07 14:29:16 -0400

    basics.h -code tweaks -updated comments

--- include/basics.h
+++ include/basics.h
@@ -29,46 +29,46 @@
 
 
 /* unsigned sized int defines */
-typedef unsigned char uint8;
-typedef unsigned short uint16;
-typedef unsigned int uint32;
-typedef unsigned long uint64;
+typedef unsigned char uint8;             /*< unsigned 8 bit integer  */
+typedef unsigned short uint16;           /*< unsigned 16 bit integer */
+typedef unsigned int uint32;             /*< unsigned 32 bit integer */
+typedef unsigned long uint64;            /*< unsigned 64 bit integer */
 
 /* signed sized int defines */
-typedef signed char int8;
-typedef signed short int16;
-typedef signed int int32;
-typedef signed long int64;
+typedef signed char int8;                /*< signed 8 bit integer  */
+typedef signed short int16;              /*< signed 16 bit integer */
+typedef signed int int32;                /*< signed 32 bit integer */
+typedef signed long int64;               /*< signed 64 bit integer */
 
 
 /* NULL define */
 #ifndef NULL
 
-#  ifdef __cplusplus
-#  define NULL (0L)
-#  else
-#  define NULL ( ( void * ) 0 )
-#  endif
+#ifdef __cplusplus
+#define NULL 0                           /*< NULL for c++ */
+#else
+#define NULL ((void *) 0)                /*< NULL for c */
+#endif
 
 #endif
 
 
 /* data pointer defines */
-typedef void* dataptr;
-typedef const void* constdataptr;
+typedef void* dataptr;                   /*< void pointer data type */
+typedef const void* constdataptr;        /*< constant void pointer data type */
 
 
 /* TRUE/FALSE defines */
 #ifndef FALSE
-#define FALSE ( 0 )
+#define FALSE ( 0 )                      /*< false define */
 #endif
 
 #ifndef TRUE
-#define TRUE (!FALSE)
+#define TRUE (!FALSE)                    /*< true define */
 #endif
 
 /* boolean define */
-typedef uint8 bool;
+typedef uint8 bool;                      /*< boolean data type */
 
 
 #endif
