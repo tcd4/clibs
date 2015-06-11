112a6b7ce130dd4a7199b186fcacc63116c1b67a
tcd4 <tcd4@njit.edu>
2015-06-11 15:17:16 -0400

    added dynamic_array.h

---
+++ include/dynamic_array.h
@@ -0,0 +1,15 @@
+/**
+ * @file dynamic_array.h
+ *
+ * @brief contains functionality for dynamic arrays
+ */
+
+#ifndef __DYNAMIC_ARRAY__
+#define __DYNAMIC_ARRAY__
+
+
+#include "basics.h"
+#include "macros.h"
+
+
+#endif
