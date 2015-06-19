635d86e1a1a082fcccbf9de5b1c0e5852aa14adb
tcd4 <tcd4@njit.edu>
2015-06-19 15:08:00 -0400

    vector.c function setups

--- types/vector.c
+++ types/vector.c
@@ -1,4 +1,125 @@
 #include "vector.h"
 
 
+/* 2d vector functions */
+void Vec2_Normalize( vec2_t *v )
+{
+}
+
+
+float Vec2_Get_Magnitude( vec2_t v )
+{
+}
+
+
+void Vec2_Set_Magnitude( vec2_t *v, float m )
+{
+}
+
+
+int8 Vec2_Magnitude_Compare( vec2_t v1, vec2_t v2 )
+{
+}
+
+
+uint32 Vec2_Distance_Between( vec2_t v1, vec2_t v2 )
+{
+}
+
+
+void Vec2_Set_Angle_Degs( vec2_t *v, float a )
+{
+}
+
+
+void Vec2_Set_Angle_Rads( vec2_t *v, float a )
+{
+}
+
+
+/* 3d vector functions */
+/*
+void Vec3_Normalize( vec3_t *v )
+{
+}
+
+
+float Vec3_Get_Magnitude( vec3_t v )
+{
+}
+
+
+void Vec3_Set_Magnitude( vec3_t *v, float m )
+{
+}
+
+
+int8 Vec3_Magnitude_Compare( vec3_t v1, vec3_t v2 )
+{
+}
+
+
+uint32 Vec3_Distance_Between( vec3_t v1, vec3_t v2 )
+{
+}
+
+
+void Vec3_Set_Angle_Degs( vec3_t *v, float a )
+{
+}
+
+
+void Vec3_Set_Angle_Rads( vec3_t *v, float a )
+{
+}
+*/
+/* 4d vector functions */
+/*
+void Vec4_Normalize( vec4_t *v )
+{
+}
+
+
+float Vec4_Get_Magnitude( vec4_t v )
+{
+}
+
+
+void Vec4_Set_Magnitude( vec4_t *v, float m )
+{
+}
+
+
+int8 Vec4_Magnitude_Compare( vec4_t v1, vec4_t v2 )
+{
+}
+
+
+uint32 Vec4_Distance_Between( vec4_t v1, vec4_t v2 )
+{
+}
+
+
+void Vec4_Set_Angle_Degs( vec4_t *v, float a )
+{
+}
+
+
+void Vec4_Set_Angle_Rads( vec4_t *v, float a )
+{
+}
+*/
+
+/* non-specific functions */
+/*
+float Get_Angle( float x, float y )
+{
+}
+
+
+void Cross_Product( vec3_t *out, vec3_t v1, vec3_t v2 )
+{
+}
+*/
+
 /*eof*/
