6cf042fa26ec2e58395434a015faa2cf7676b3a3
tcd4 <tcd4@njit.edu>
2015-06-22 14:52:46 -0400

    queue.c - setup

--- types/queue.c
+++ types/queue.c
@@ -1,4 +1,39 @@
 #include "queue.h"
 
 
+Queue* New_Queue()
+{
+}
+
+
+void Push_Queue( Queue *queue, dataptr data )
+{
+}
+
+
+dataptr Pop_Queue( Queue *queue )
+{
+}
+
+
+void Clear_Queue( Queue *queue )
+{
+}
+
+
+bool Queue_Is_Empty( Queue *queue )
+{
+}
+
+
+Queue* Duplicate_Queue( Queue *queue )
+{
+}
+
+
+void Free_Queue( Queue *queue )
+{
+}
+
+
 /*eof*/
