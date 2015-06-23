d05abdfe41bf5969d61f90a38fd171f30777bfef
tcd4 <tcd4@njit.edu>
2015-06-23 15:14:21 -0400

    stack.h - added

---
+++ include/stack.h
@@ -0,0 +1,17 @@
+/**
+ * @file stack.h
+ *
+ * @brief contains support for stacks
+ */
+
+
+#ifndef __STACK__
+#define __STACK__
+
+
+#include "linked_list.h"
+#include "macros.h"
+#include "basics.h"
+
+
+#endif
