8dc5a9226987039295ecded881f232d993379368
tcd4 <tcd4@njit.edu>
2015-06-23 14:31:00 -0400

    queue.c - updated - functions New_Queue, Push_Queue, and Pop_Queue

--- types/queue.c
+++ types/queue.c
@@ -3,16 +3,53 @@
 
 Queue* New_Queue()
 {
+    Queue *new;
+
+    new = C_New( Queue, 1 );
+    Return_Value_If_Fail( new, NULL );
+
+    new->head = NULL;
+    new->tail = NULL;
+    new->length = 0;
+
+    return new;
 }
 
 
 void Push_Queue( Queue *queue, dataptr data )
 {
+    List *new;
+
+    Return_If_Fail( queue );
+
+    if( !queue->head )
+    {
+	new = New_List();
+	Return_If_Fail( new );
+	new->data = data;
+    }
+    else
+    {
+	new = Append_To_List( queue->head, data );
+	Return_If_Fail( new );
+    }
+
+    queue->head = new;
+    queue->tail = End_Of_List( queue->head );
+    queue->length++;
 }
 
 
 dataptr Pop_Queue( Queue *queue )
 {
+    dataptr temp;
+
+    temp = queue->head->data;
+
+    queue->head = Remove_List_Segment( queue->head, queue->head );
+    queue->length--;
+
+    return temp;
 }
 
 
