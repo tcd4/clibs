1b2c1be832cfd0c605ae1d449e13e600110ccdd1
tcd4 <tcd4@njit.edu>
2015-06-23 15:32:45 -0400

    stack.h - updated - struct added - functions added

--- include/stack.h
+++ include/stack.h
@@ -14,4 +14,61 @@
 #include "basics.h"
 
 
+/*< the stack struct */
+typedef struct
+{
+    DList    *head;    /*< the first element of the stack */
+    DList    *tail;    /*< the last element of the stack */
+    uint64   length;   /*< the number of elements in the stack */
+}Stack;
+
+
+/**
+ * @brief creates a new Stack
+ *
+ * @return a pointer to the new Stack
+ */
+Stack* New_Stack();
+
+/**
+ * @brief pushes data onto a Stack
+ *
+ * @param stack the Stack to use
+ * @param data the data to push onto the Stack
+ */
+void Push_Stack( Stack *stack, dataptr data );
+
+/**
+ * @brief removes and returns the data from the last element of the Stack
+ *
+ * @param stack the Stack to use
+ *
+ * @return a pointer to the data of the last element of the Stack
+ */
+dataptr Pop_Stack( Stack *stack );
+
+/**
+ * @brief clears the elements of a Stack
+ *
+ * @param stack the Stack to use
+ */
+void Clear_Stack( Stack *stack );
+
+/**
+ * @brief checks if a Stack is empty
+ *
+ * @param stack the Stack to use
+ *
+ * @return TRUE if the Stack is empty, False if else
+ */
+bool Stack_Is_Empty( Stack *stack );
+
+/**
+ * @brief frees the memory of a Stack
+ *
+ * @param stack the Stack to use
+ */
+void Free_Stack( Stack *stack );
+
+
 #endif
