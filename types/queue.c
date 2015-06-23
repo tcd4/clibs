b743a781a7288a19268d710c599c49e14465f518
tcd4 <tcd4@njit.edu>
2015-06-23 14:46:04 -0400

    queue.c - updated - functions Clear_Queue, Queue_Is_Empty, and Free_Queue

--- types/queue.c
+++ types/queue.c
@@ -55,11 +55,20 @@ dataptr Pop_Queue( Queue *queue )
 
 void Clear_Queue( Queue *queue )
 {
+    Return_If_Fail( queue );
+
+    Free_List( &queue->head );
+
+    queue->tail = NULL;
+    queue->length = 0;
 }
 
 
 bool Queue_Is_Empty( Queue *queue )
 {
+    Return_Value_If_Fail( queue->length != 0, FALSE );
+    
+    return TRUE;
 }
 
 
@@ -68,8 +77,18 @@ Queue* Duplicate_Queue( Queue *queue )
 }
 
 
-void Free_Queue( Queue *queue )
+void Free_Queue( Queue **queue )
 {
+    Queue *temp;
+    
+    Return_If_Fail( queue );
+    
+    temp = *queue;
+    Return_If_Fail( temp );
+
+    Free_List( &temp->head );
+    free( temp );
+    *queue = NULL;
 }
 
 
