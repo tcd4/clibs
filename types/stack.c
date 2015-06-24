c85994c914f133ac7490a12ee437a0b429350fbd
tcd4 <tcd4@njit.edu>
2015-06-24 14:14:01 -0400

    stack.c - updated - functions setup

--- types/stack.c
+++ types/stack.c
@@ -1,3 +1,34 @@
 #include "stack.h"
 
+
+Stack* New_Stack()
+{
+}
+
+
+void Push_Stack( Stack *stack, dataptr data )
+{
+}
+
+
+dataptr Pop_Stack( Stack *stack )
+{
+}
+
+
+void Clear_Stack( Stack *stack )
+{
+}
+
+
+bool Stack_Is_Empty( Stack *stack )
+{
+}
+
+
+void Free_Stack( Stack *stack )
+{
+}
+
+
 /*eof*/
